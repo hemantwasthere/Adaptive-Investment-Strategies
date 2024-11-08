85aa4dde349410d04d3f365dd28adda76e9b21c2
Hemant <hrsmart2150@gmail.com>
2024-11-08 09:59:41 +0530

    feat: added copy address button

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -7,11 +7,17 @@ import {
   useStarkProfile,
 } from "@starknet-react/core";
 import { motion } from "framer-motion";
-import { ChevronDown } from "lucide-react";
+import {
+  Check,
+  CheckCircle,
+  ChevronDown,
+  CircleCheck,
+  Copy,
+} from "lucide-react";
 import Image from "next/image";
 import React from "react";
 
-import { buttonVariants } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -19,11 +25,20 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Skeleton } from "@/components/ui/skeleton";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import { cn, shortAddress, truncate } from "@/lib/utils";
 
+import { toast } from "@/hooks/use-toast";
 import MaxWidthWrapper from "./max-width-wrapper";
 
 const Navbar: React.FC = () => {
+  const [showTick, setShowTick] = React.useState(false);
+
   const { address, isConnecting } = useAccount();
   const { connect, connectors } = useConnect();
   const { disconnectAsync } = useDisconnect();
@@ -54,82 +69,121 @@ const Navbar: React.FC = () => {
           />
         </div>
 
-        <DropdownMenu>
-          <DropdownMenuTrigger className="group select-none rounded-md text-white transition-all focus-visible:outline-0 focus-visible:ring-0">
-            {!address && isConnecting && (
-              <div className="flex h-9 w-[9.5rem] items-center justify-center gap-2 rounded-md border border-accent/10 group-focus-visible:outline-1 group-focus-visible:outline-white">
-                <Image
-                  src={starkProfile?.profilePicture || "/fallback-avatar.svg"}
-                  className="shrink-0 rounded-full"
-                  width={20}
-                  height={20}
-                  alt="skeleton-pfp"
-                />
-                <p className="flex items-center gap-1 text-sm">
-                  <Skeleton className="h-5 w-full" />
-                  <ChevronDown className="size-3" />
-                </p>
-              </div>
-            )}
-
-            {address && !isConnecting && (
-              <div className="flex h-9 w-[9.5rem] items-center justify-center gap-2 rounded-md border border-accent/10 group-focus-visible:outline-1 group-focus-visible:outline-white">
-                <Image
-                  src={starkProfile?.profilePicture || "/fallback-avatar.svg"}
-                  className="shrink-0 rounded-full"
-                  width={20}
-                  height={20}
-                  alt="strkprofile-pfp"
-                />
-                <p className="flex items-center gap-1 text-sm">
-                  {starkProfile && starkProfile.name
-                    ? truncate(starkProfile.name, 6, 6)
-                    : shortAddress(address, 4, 4)}
-                  <ChevronDown className="size-3" />
-                </p>
-              </div>
-            )}
-
-            {!address && !isConnecting && (
-              <p
-                className={cn(
-                  buttonVariants(),
-                  "flex w-[9.5rem] select-none items-center justify-center gap-1 text-sm",
-                )}
-              >
-                Connect Wallet <ChevronDown className="!size-3" />
-              </p>
-            )}
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="min-w-[9rem] border border-accent/10 bg-primary/90 text-white">
-            {!address ? (
-              connectors?.map((connector) => (
-                <DropdownMenuItem
-                  key={connector.id}
-                  className="hover:!bg-accent/10 hover:!text-white"
-                  onClick={() => {
-                    connect({ connector });
-                  }}
-                >
+        <div className="flex items-center gap-2">
+          <DropdownMenu>
+            <DropdownMenuTrigger className="group select-none rounded-md text-white transition-all focus-visible:outline-0 focus-visible:ring-0">
+              {!address && isConnecting && (
+                <div className="flex h-9 w-[9.5rem] items-center justify-center gap-2 rounded-md border border-accent/10 group-focus-visible:outline-1 group-focus-visible:outline-white">
                   <Image
-                    src={connector.icon as string}
-                    width={15}
-                    height={15}
-                    alt="icon"
+                    src={starkProfile?.profilePicture || "/fallback-avatar.svg"}
+                    className="shrink-0 rounded-full"
+                    width={20}
+                    height={20}
+                    alt="skeleton-pfp"
                   />
-                  <p>{connector.name}</p>
+                  <p className="flex items-center gap-1 text-sm">
+                    <Skeleton className="h-5 w-full" />
+                    <ChevronDown className="size-3" />
+                  </p>
+                </div>
+              )}
+
+              {address && !isConnecting && (
+                <div className="flex h-9 w-[9.5rem] items-center justify-center gap-2 rounded-md border border-accent/10 group-focus-visible:outline-1 group-focus-visible:outline-white">
+                  <Image
+                    src={starkProfile?.profilePicture || "/fallback-avatar.svg"}
+                    className="shrink-0 rounded-full"
+                    width={20}
+                    height={20}
+                    alt="strkprofile-pfp"
+                  />
+                  <p className="flex items-center gap-1 text-sm">
+                    {starkProfile && starkProfile.name
+                      ? truncate(starkProfile.name, 6, 6)
+                      : shortAddress(address, 4, 4)}
+                    <ChevronDown className="size-3" />
+                  </p>
+                </div>
+              )}
+
+              {!address && !isConnecting && (
+                <p
+                  className={cn(
+                    buttonVariants(),
+                    "flex w-[9.5rem] select-none items-center justify-center gap-1 text-sm",
+                  )}
+                >
+                  Connect Wallet <ChevronDown className="!size-3" />
+                </p>
+              )}
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="min-w-[9rem] border border-accent/10 bg-primary/90 text-white">
+              {!address ? (
+                connectors?.map((connector) => (
+                  <DropdownMenuItem
+                    key={connector.id}
+                    className="hover:!bg-accent/10 hover:!text-white"
+                    onClick={() => {
+                      connect({ connector });
+                    }}
+                  >
+                    <Image
+                      src={connector.icon as string}
+                      width={15}
+                      height={15}
+                      alt="icon"
+                    />
+                    <p>{connector.name}</p>
+                  </DropdownMenuItem>
+                ))
+              ) : (
+                <DropdownMenuItem
+                  onClick={() => disconnectAsync()}
+                  className="hover:!bg-accent/10 hover:!text-white"
+                >
+                  Disconnect
                 </DropdownMenuItem>
-              ))
-            ) : (
-              <DropdownMenuItem
-                onClick={() => disconnectAsync()}
-                className="hover:!bg-accent/10 hover:!text-white"
-              >
-                Disconnect
-              </DropdownMenuItem>
-            )}
-          </DropdownMenuContent>
-        </DropdownMenu>
+              )}
+            </DropdownMenuContent>
+          </DropdownMenu>
+
+          {address && (
+            <TooltipProvider delayDuration={0}>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Button
+                    className="group size-8 cursor-pointer transition-all hover:bg-primary"
+                    variant="ghost"
+                    onClick={() => {
+                      navigator.clipboard.writeText(address);
+                      toast({
+                        description: (
+                          <div className="flex items-center gap-2 text-white">
+                            <CircleCheck className="size-5" />
+                            Address copied
+                          </div>
+                        ),
+                      });
+                      setShowTick(true);
+                      setTimeout(() => setShowTick(false), 1000);
+                    }}
+                  >
+                    {!showTick && (
+                      <Copy className="size-3 text-accent/30 transition-all group-hover:text-accent" />
+                    )}
+
+                    {showTick && (
+                      <Check className="size-3 text-accent/30 transition-all group-hover:text-accent" />
+                    )}
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent className="border border-accent/10">
+                  copy address
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          )}
+        </div>
       </MaxWidthWrapper>
     </header>
   );
