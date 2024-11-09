210b4dae9046af3baf7572aa9d4a5111e818658d
Hemant <hrsmart2150@gmail.com>
2024-11-09 16:52:44 +0530

    chore: added demo code for contract interaction

--- client/src/components/deposit.tsx
+++ client/src/components/deposit.tsx
@@ -3,11 +3,12 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useAccount, useBalance } from "@starknet-react/core";
 import { motion } from "framer-motion";
+import { Info } from "lucide-react";
 import React from "react";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 
-import { Button, buttonVariants } from "@/components/ui/button";
+import { buttonVariants } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -19,7 +20,6 @@ import {
 import { Input } from "@/components/ui/input";
 import { toast } from "@/hooks/use-toast";
 import { cn } from "@/lib/utils";
-import { Info } from "lucide-react";
 
 const formSchema = z.object({
   ethAmount: z.string().refine(
@@ -58,6 +58,32 @@ const Deposit: React.FC = () => {
   React.useEffect(() => {
     if (!address) setEthAmount("");
   }, [address]);
+
+  // const PRIVATE_KEY =
+  //   "0x0574ba4998dd9aedf1c4d6e56b747b29256a795bc3846437d121cd64b972bdd8";
+
+  // const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS!;
+  // const provider = new RpcProvider({
+  //   nodeUrl: "http://0.0.0.0:5050/rpc",
+  // });
+  // const account = new Account(
+  //   provider,
+  //   "0x05d3a8f378500497479d3a16cfcd54657246dc37da8270b52e49319fac139939",
+  //   PRIVATE_KEY,
+  // );
+  // const contract = new Contract(abi, CONTRACT_ADDRESS, account);
+
+  // async function callContract() {
+  //   const res = await contract.get_balance();
+  //   console.log(res.toString());
+  // }
+
+  // async function writeToContract() {
+  //   contract.connect(account);
+  //   const myCall = contract.populate("increase_balance", [35]);
+  //   const res = await contract.increase_balance(myCall.calldata);
+  //   console.log(res);
+  // }
 
   return (
     <div className="h-full">
